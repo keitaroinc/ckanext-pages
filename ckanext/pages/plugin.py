df24c0c9618ac75e07be6f378056e461c0672c80
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2018-08-04 23:45:13 +0200

    Fix cherry-pick conflict

--- ckanext/pages/plugin.py
+++ ckanext/pages/plugin.py
@@ -99,6 +99,7 @@ class PagesPlugin(PagesPluginBase):
     p.implements(p.IRoutes, inherit=True)
     p.implements(p.IActions, inherit=True)
     p.implements(p.IAuthFunctions, inherit=True)
+    p.implements(p.IValidators)
 
 
     def update_config(self, config):
@@ -218,8 +219,12 @@ class PagesPlugin(PagesPluginBase):
             'ckanext_group_pages_list': auth.group_pages_list,
        }
 
-class TextBoxView(p.SingletonPlugin):
+    def get_validators(self):
+        return {
+            'convert_date_to_iso': actions.convert_date_to_iso
+        }
 
+class TextBoxView(p.SingletonPlugin, DefaultTranslation):
     p.implements(p.IConfigurer, inherit=True)
     p.implements(p.IResourceView, inherit=True)
 
