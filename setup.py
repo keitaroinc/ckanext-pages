b7d4287866aa58da24b01f30ce75bfbd7bf42b16
Goran Nushkov <gnuskov@protonmail.com>
2019-05-13 00:42:32 +0200

    Fixed setup.py for wheel compiling

--- setup.py
+++ setup.py
@@ -52,34 +52,12 @@ setup(
     namespace_packages=['ckanext'],
     include_package_data=True,
     package_data={
-            '': ['theme/*/*.html', 'theme/*/*/*.html', 'theme/*/*/*/*.html'],
+            '': ['theme/*/*.html', 'theme/*/*/*.html', 'theme/*/*/*/*.html'] + i18n_files,
     },
     zip_safe=False,
     install_requires=[
         # -*- Extra requirements: -*-
     ],
-    entry_points=\
-    """
-	name='ckanext-pages',
-	version=version,
-	description='Basic CMS extension for ckan',
-	long_description='',
-	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	keywords='',
-	author='David Raznick',
-	author_email='david.raznick@gokfn.org',
-	url='',
-	license='',
-	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-	namespace_packages=['ckanext'],
-	include_package_data=True,
-	package_data={
-            '': ['theme/*/*.html', 'theme/*/*/*.html', 'theme/*/*/*/*.html'] + i18n_files,
-	},
-	zip_safe=False,
-	install_requires=[
-		# -*- Extra requirements: -*-
-	],
 	entry_points=\
 	"""
         [ckan.plugins]
